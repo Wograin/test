0111686088079d6c588bc5366668a25636806e7d
Andrey <enkilart@mail.ru>
2019-04-18 20:32:09 +0300

    Это наш первый комит

---
+++ 1_lesson/1_task/js/js_lesson_1.js
@@ -0,0 +1,22 @@
+function firstTask() {
+    'use strict';
+    let money = prompt("Ваш бюджет на месяц?", "8164");
+    let time = prompt("Введите дату в формате YYYY-MM-DD", "2019-04-18");
+    let appData = {
+        "budget": money,
+        "timeData": time,
+        "expenses": {},
+        "optionalExpenses": {},
+        "income": [],
+        "savings": false
+    };
+    
+    let utilityFee = prompt("Введите обязательную статью расходов в этом месяце", "Плата за коммунальные услуги");
+    let salary = prompt("Во сколько обойдется?", "2200");
+    
+    appData.expenses[utilityFee] = salary;
+    
+    alert(money / 30);    
+}
+
+firstTask();
