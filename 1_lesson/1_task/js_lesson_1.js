ad5665d712f4ae76f9c1964ea52ac62858e9332b
Andrey <enkilart@mail.ru>
2019-04-18 23:10:02 +0300

    Переделанный урок

--- 1_lesson/1_task/js_lesson_1.js
+++ 1_lesson/1_task/js_lesson_1.js
@@ -13,7 +13,12 @@ function firstTask() {
     
     let utilityFee = prompt("Введите обязательную статью расходов в этом месяце", "Плата за коммунальные услуги");
     let salary = prompt("Во сколько обойдется?", "2200");
-    
+
+    appData.expenses[utilityFee] = salary;
+
+    utilityFee = prompt("Введите обязательную статью расходов в этом месяце", "Продукты питания");
+    salary = prompt("Во сколько обойдется?", "1800");
+
     appData.expenses[utilityFee] = salary;
     
     alert(money / 30);    
