14e6033413f444e999e08a723ccbdfa132d06b64
Andrey <enkilart@mail.ru>
2019-04-20 15:30:23 +0300

    Второй урок: основное задание

---
+++ 2_lesson/1_task/1_цикл_for_/js_lesson_2_(for).js
@@ -0,0 +1,42 @@
+function firstTask() {
+    'use strict';
+    
+    let money = +prompt("Ваш бюджет на месяц?", "8164");
+    let time = prompt("Введите дату в формате YYYY-MM-DD", "2019-04-18");
+    let appData = {
+        "budget": money,
+        "timeData": time,
+        "expenses": {},
+        "optionalExpenses": {},
+        "income": [],
+        "savings": false
+    };
+
+    for (let i = 0; i < 2; i++) {
+        let utilityFee = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            salary = prompt("Во сколько обойдется?", ""); 
+        
+        if ( typeof(utilityFee) === "string" && utilityFee != null && utilityFee != "" && salary != null && salary != "" ) {
+            console.log("done");
+            appData.expenses[utilityFee] = salary;
+        } else {
+           i--;
+        }
+    }
+
+    appData.monetPerDay = appData.budget / 30;
+
+    alert("Ежедневный бюджет: " + appData.monetPerDay);
+    
+    if(appData.monetPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.monetPerDay > 100 && appData.monetPerDay < 700) {
+        console.log("Средний уровень достатка");
+    } else if (appData.monetPerDay > 700){
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("ИДИ РАБОТАТЬ ! ! !");
+    }
+}
+
+firstTask();
