20b612b7dcac0d6a9f9f18396533989cd414d4ad
Andrey <enkilart@mail.ru>
2019-05-03 23:51:48 +0300

    Урок 9: Основное задание

---
+++ 9_lesson/2_task/js_lesson_9.js
@@ -0,0 +1,8 @@
+let age = document.getElementById('age');
+console.log(age);
+function showUser(surname, name) {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+
+let d = showUser.bind(age);
+d("Снежек", "Джон");
