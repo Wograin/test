3bc7e6871e830ec8abafeb828da4b15db26ec69c
Andrey <enkilart@mail.ru>
2019-05-15 20:12:33 +0300

    Урок 13: Основное задание

--- 13_lesson_(слайдер и калькулятор на чистом JS)/js/script.js
+++ 13_lesson_(слайдер и калькулятор на чистом JS)/js/script.js
@@ -295,16 +295,18 @@ window.addEventListener("DOMContentLoaded", () => {
         totalValue = document.getElementById("total"),
         personsSum = 0,
         daysSum = 0,
-        total = 0;
+        total = 0,
+        koef = 1;
 
     totalValue.textContent = 0;
 
     persons.addEventListener("input", function () {
+
         this.value = this.value.replace(/[^+0-9]/ig, "");
         personsSum = +this.value;
 
         if (daysSum != "" && personsSum != "") {
-            total = (daysSum + personsSum) * 4000;
+            total = (daysSum + personsSum) * 4000 * koef;
             if (restDays.value == "") {
                 totalValue.textContent = 0;
             } else {
@@ -316,11 +318,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     restDays.addEventListener("input", function () {
+
         this.value = this.value.replace(/[^+0-9]/ig, "");
         daysSum = +this.value;
 
         if (daysSum != "" && personsSum != "") {
-            total = (daysSum + personsSum) * 4000;
+            total = (daysSum + personsSum) * 4000 * koef;
             if (persons.value == "") {
                 totalValue.textContent = 0;
             } else {
@@ -332,11 +335,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     place.addEventListener("input", function () {
+        koef = this.options[this.selectedIndex].value;
         if (restDays.value == "" || persons.value == "") {
             totalValue.textContent = 0;
         } else {
-            let a = total;
-            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            total = (daysSum + personsSum) * 4000 * koef;
+            totalValue.innerHTML = total;
         }
     });
 
